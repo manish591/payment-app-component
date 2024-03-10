27a5e37ceb4de0856a4ac4a3d42a80f81ea5067d
Manish Devrani <manishdevrani777@gmail.com>
2024-03-10 23:18:52 +0530

    added cors

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,15 @@
 import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
 import router from "./routes";
 
 const app = express();
+
+app.use(cors({
+  origin: "*",
+}));
+
+app.use(express.json());
+app.use(express.urlencoded());
 
 app.use("/", router);
 
