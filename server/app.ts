2d6682805913d05b3b64aa39690ac9b5551df4ec
Manish Devrani <manishdevrani777@gmail.com>
2024-03-10 22:58:55 +0530

    added global catch and 404 error handler

--- server/app.ts
+++ server/app.ts
@@ -1,9 +1,22 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import router from "./routes";
 
 const app = express();
 
-app.get("/", function(req, res) {
-  res.send("data");
+app.use("/", router);
+
+app.use(function(req: Request, res: Response, next: NextFunction){
+  next(new Error("404 Not Found!"));
+});
+
+app.use(function(err: Error, req: Request, res: Response, next: NextFunction) {
+  if(err) {
+    res.status(500).json({
+      message: err.message
+    });
+  } else {
+    next();
+  }
 });
 
 export default app;
