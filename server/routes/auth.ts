332a74c76849e83a0f2f08ffe7082be09dd2da06
Manish Devrani <manishdevrani777@gmail.com>
2024-04-27 23:27:10 +0530

    updated route name

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -5,7 +5,7 @@ import validator from "../middlewares/validators/users";
 const router = Router();
 
 router.post("/signup", validator.validateSignupData, users.signup);
-router.post("/login", validator.validateLoginData, users.login);
+router.post("/signin", validator.validateLoginData, users.login);
 router.post("/singout", users.signout);
 
 export default router;
